fc5d10ad145f3efc3688da5432697001d09ba910
umedoblock <devnull@localhost>
2012-10-04 03:10:57 +0900

    directory 構造を刷新。 (refs #85)

---
+++ umatobi/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ['simulator', 'screen']
+
+__version__ = '0.0'     # See also below and setup.py
+__revision__ = "$Id$"
+
+# New software should look at this instead of at __version__ above.
+version_info = (0, 0, 0, 'experimental', 0)    # See also above and setup.py
---
+++ umatobi/simulator/__init__.py
@@ -0,0 +1,1 @@
+import umatobi.screen
