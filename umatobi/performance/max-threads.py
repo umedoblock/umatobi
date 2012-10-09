389d392517574080309b0adfb5811397214d5aea
umedoblock <devnull@localhost>
2012-10-10 06:01:46 +0900

    作成できる thread 数を確認した。 以下の通り 381 threads であった。 len(threads) = 381 (refs #106)

---
+++ umatobi/performance/max-threads.py
@@ -0,0 +1,31 @@
+import threading
+import time
+import _thread # for except _thread.error
+
+class LimitedThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.die_out = False
+
+    def run(self):
+        while not self.die_out:
+            time.sleep(0.01)
+
+threads = []
+for i in range(100000000):
+    thread = LimitedThread()
+    try:
+        thread.start()
+    except _thread.error as raiz:
+        if raiz.args[0] == "can't start new thread":
+            break
+    threads.append(thread)
+
+# print('dir(threads[0]) =')
+# print(dir(threads[0]))
+print('len(threads) =', len(threads))
+# help(threads[0])
+
+for thread in threads:
+    thread.die_out = True
+    thread.join()
