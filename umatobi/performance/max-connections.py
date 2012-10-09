23399a6a1b291e226329b91c55a74a4c1f2dbe28
umedoblock <devnull@localhost>
2012-10-10 03:52:29 +0900

    max connections は 1021 が正しいようでした。 sqlite3.connect() + open('a', 'wb') = 1021 のように、それぞれの connection 数を併せて1021が限界のようでした。 (refs #106)

--- umatobi/performance/max-connections.py
+++ umatobi/performance/max-connections.py
@@ -3,16 +3,48 @@ import sys
 import sqlite3
 
 conns = []
+connfmt = '/tmp/sqlite3-open-max-{}.db'
 for i in range(100000000):
     try:
-        conn = sqlite3.connect('/tmp/sqlite3-open-max-{}.db'.format(i))
+        conn = sqlite3.connect(connfmt.format(i))
     except sqlite3.OperationalError as raiz:
+#       print('raiz =', raiz)
+#       print('raiz.args =', raiz.args)
         if raiz.args[0] == 'unable to open database file':
-            print('max connections are {}.'.format(i - 1))
-            for j in range(i):
-                os.remove('/tmp/sqlite3-open-max-{}.db'.format(j))
-            sys.exit(0)
+            break
         else:
             raise raiz
 
     conns.append(conn)
+#   if len(conns) > 1010:
+#       break
+
+print('sqlite3.connect() max connections are {}.'.format(len(conns)))
+
+fs = []
+ffmt = '/tmp/file-open-max-{}.bin'
+for i in range(100000000):
+    try:
+        f = open(ffmt.format(i), 'wb')
+    except IOError as raiz:
+#       print('raiz =', raiz)
+#       print('raiz.args =', raiz.args)
+        if raiz.args == (24, 'Too many open files'):
+            break
+        else:
+            raise raiz
+
+    fs.append(f)
+
+print('open() max are {}.'.format(len(fs)))
+
+for i, conn in enumerate(conns):
+    conn.close()
+    os.remove(connfmt.format(i))
+
+for i, f in enumerate(fs):
+    f.close()
+    os.remove(ffmt.format(i))
+
+print('sqlite3.conn() max plus open() max are {}.'.
+    format(len(conns) + len(fs)))
