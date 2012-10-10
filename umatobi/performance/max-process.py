1e39866f71e856b415bd3909a5d0d2d6f134bedf
umedoblock <devnull@localhost>
2012-10-10 10:21:12 +0900

    上々の結果であった。 1400個くらいのprocessを作れた。 もっともっと作成できそうではあったが、swapdが働き始めてとても重たくなった。 実用には耐えない感じであった。 しかし、30個弱のprocessを作れれば良いのだから、気にする必要はないかな？ 381 threads 制限が 1 process に対する制限でありますように。 (refs #106)

---
+++ umatobi/performance/max-process.py
@@ -0,0 +1,32 @@
+# 16.3. multiprocessing — Process-based parallelism
+# 16.3.1.1. The Process class
+
+from multiprocessing import Process
+import os
+import time
+
+def info(title):
+    print(title)
+    print('module name:', __name__)
+    print('parent process:', os.getppid())
+    print('process id:', os.getpid())
+    print()
+
+def f(name):
+    info('function f')
+    print('hello', name)
+    time.sleep(2000)
+
+if __name__ == '__main__':
+    info('main line')
+    ps = []
+    for i in range(100000000):
+        p = Process(target=f, args=('bob',))
+        p.start()
+        ps.append(p)
+        print('i =', i)
+        if i > 100:
+            break
+
+    for i, p in enumerate(ps):
+        p.join()
