41bd4d46620d44be9af9de06e66485edecce66aa
umedoblock <devnull@localhost>
2012-10-10 14:20:29 +0900

    thread を 10000 個以上作れる。 正確には、total_threads = 11322 (refs #106)

--- umatobi/performance/max-threads-in-system.py
+++ umatobi/performance/max-threads-in-system.py
@@ -7,11 +7,10 @@ import _thread # for except _thread.error
 class LimitedThread(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        self.die_out = False
+        self.die_out = threading.Event()
 
     def run(self):
-        while not self.die_out:
-            time.sleep(0.01)
+        self.die_out.wait()
 
 def info(title):
     print(title)
@@ -37,15 +36,15 @@ def make_many_threads_in_process(no, state):
     print('no={}, created {} threads in a process(pid={}).'.format(no, len(threads), os.getpid()))
     print('no={}, id(state)=0x{:08x} in make_many_threads_in_process()'.format(no, id(state)))
 
-  # created_thread_num = len(threads)
-    state.len_thread.value = len(threads) * 1000 + state.no
+    state.len_thread.value = len(threads)
     state.create_threads.set()
 
+    print('no={} waiting... set leave_there to SIGNAL.'.format(state.no))
     state.leave_there.wait()
     state.init = 'in make_many_threads_in_process()'
 
     for thread in threads:
-        thread.die_out = True
+        thread.die_out.set()
         thread.join()
 
 class State(object):
@@ -63,9 +62,7 @@ if __name__ == '__main__':
     leave_there = multiprocessing.Event()
     total_threads = 0
 
-    for i in range(2):
-        len_thread = multiprocessing.Value('i', 0)
-
+    for i in range(34):
         state = State(i, leave_there)
         print('no={}, id(state)=0x{:08x} in __main__'.format(state.no, id(state)))
         p = multiprocessing.Process(target=make_many_threads_in_process, args=(i, state))
@@ -74,17 +71,19 @@ if __name__ == '__main__':
         ps.append(p)
         states.append(state)
 
-    leave_there.set()
-
     for i, p in enumerate(ps):
         states[i].create_threads.wait()
         print('no={}, len_thread.value={}'.format(states[i].no, states[i].len_thread.value))
         print('no={} state.init = {}.'.format(states[i].no, states[i].init))
         total_threads += states[i].len_thread.value
 
+    print('total_threads =', total_threads)
+  # time.sleep(30)
+
+    leave_there.set()
+
     for i, p in enumerate(ps):
         p.join()
         print('no={} prcess done.'.format(i))
 
     print()
-    print('total_threads =', total_threads)
