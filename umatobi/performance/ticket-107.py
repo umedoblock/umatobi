e1183bcf398b5f4973e8eb63d688794f67d5c104
umedoblock <devnull@localhost>
2012-10-20 05:01:29 +0900

    8192 個の thread を作成できることを確認。 (refs #107)

---
+++ umatobi/performance/ticket-107.py
@@ -0,0 +1,99 @@
+import multiprocessing
+import socket
+import os
+import sys
+import threading
+import time
+import _thread # for except _thread.error
+
+class LimitedThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        try:
+            self.udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        except socket.error as raiz:
+            if raiz.args == (24, 'Too many open files') or \
+               raiz.args == (23, 'Too many open files in system'):
+               print('LimitedThread reached limit.')
+
+        self.die_out = threading.Event()
+
+    def run(self):
+        self.die_out.wait()
+
+def info(title):
+    print(title)
+    print('module name:', __name__)
+    print('parent process:', os.getppid())
+    print('process id:', os.getpid(), file=sys.stderr)
+    print()
+
+def make_many_threads_in_process(no, state):
+  # info('make_many_threads_in_process(no={})'.format(no))
+
+    threads = []
+    for i in range(256):
+        thread = LimitedThread()
+        try:
+            thread.start()
+        except _thread.error as raiz:
+            if raiz.args == ("can't start new thread",):
+                break
+        threads.append(thread)
+
+    state.len_threads.value = len(threads)
+    state.create_threads.set()
+
+    state.leave_there.wait() # multiprocessing.Event()
+    state.init = 'cannot change in make_many_threads_in_process()'
+
+    for thread in threads:
+        thread.die_out.set()
+    for thread in threads:
+        thread.join()
+
+class State(object):
+    def __init__(self, no, leave_there):
+        self.no = no
+        self.leave_there = leave_there # multiprocessing.Event()
+        self.len_threads = multiprocessing.Value('i', 0)
+        self.create_threads = multiprocessing.Event()
+        self.init = 'in main'
+
+if __name__ == '__main__':
+    info('main line')
+    ps = []
+    states = []
+    leave_there = multiprocessing.Event()
+    total_threads = 0
+
+    first_len_threads = 0
+    for i in range(32):
+        state = State(i, leave_there)
+      # print('i =', i, file=sys.stderr)
+      # print('no={}, id(state)=0x{:08x} in __main__'.format(state.no, id(state)))
+        p = multiprocessing.Process(target=make_many_threads_in_process, args=(i, state))
+        p.start()
+
+        ps.append(p)
+        state.create_threads.wait()
+        total_threads += state.len_threads.value
+
+        states.append(state)
+        if first_len_threads == 0:
+            first_len_threads = state.len_threads.value
+        elif first_len_threads == state.len_threads.value:
+            pass
+        else:
+            break
+
+    print('total_threads =', total_threads)
+  # time.sleep(30)
+
+    leave_there.set() # multiprocessing.Event()
+
+    for i, p in enumerate(ps):
+        p.join()
+      # print('no={} prcess done.'.format(i))
+
+    print()
