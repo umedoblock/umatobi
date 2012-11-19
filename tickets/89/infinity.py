331c832b14cf4033cd007822df95c72dedde3cc3
umedoblock <devnull@localhost>
2012-11-19 10:46:23 +0900

    極限取れるようにした。 やはり、確率は収束するようだ。 数学的な証明はまだないが。。。 なんだっけ？convergence, divergence？ 収束することを、ちゃんと証明してみたいな。 (refs #89)

--- tickets/89/infinity.py
+++ tickets/89/infinity.py
@@ -21,38 +21,42 @@ if __name__ == '__main__':
     # data_blocks = n_slots * n_blocks
     data_blocks = 128
     pb = 0.99
+    max_redundancy = 64
 
-    total_blocks = data_blocks * rate
-
-    n_slots_tup = range(2, 4 + 1, 2)
-    n_slots_tup = range(2, 16 + 1, 2)
     point_styles = ['o', 'D', 's', '+'] * 2
     index = 0
-    for n_slots in n_slots_tup:
-        n_blocks = data_blocks / n_slots
-        ps = pb ** n_blocks
-        print('n_blocks={}'.format(n_blocks))
-        print('n_slots={}'.format(n_slots))
-        print('pb={:.3f}'.format(pb))
-        print('ps={:.3f}'.format(ps))
-
-        xs = np.arange(n_slots, n_slots * rate + 1.0)
+    for n_parity_slots in range(0, 4 + 1):
+        xs = np.arange(1, max_redundancy + 1)
         ys = []
         for x in xs:
-            n_parity_slots = x - n_slots
-            pd = formula.p_reed_solomon(int(x), int(n_parity_slots), ps)
+            n_slots = x
+            if n_slots <= n_parity_slots:
+                continue
+            if n_slots == 0:
+                raise RuntimeError('n_slots must be positive integer.')
+            n_blocks = data_blocks / n_slots
+            ps = pb ** n_blocks
+          # print('n_slots=x={}'.format(x))
+          # print('n_blocks={}'.format(n_blocks))
+          # print('n_slots={}'.format(n_slots))
+          # print('pb={:.3f}'.format(pb))
+          # print('ps={:.3f}'.format(ps))
+          # print()
+
+            pd = formula.p_reed_solomon(int(n_slots), int(n_parity_slots), ps)
             ys.append(pd)
-        xs = xs[:len(ys)]
-        xs /= n_slots
+
+        xs = xs[len(xs)-len(ys):]
         plt.plot(xs, ys, 'b{}'.format(point_styles[index]))
+      # plt.plot(xs, ys, lw=1, color='b')
         index += 1
+      # print('len(xs) =', len(xs))
+      # print('len(ys) =', len(ys))
       # print('xs =', list(xs))
       # print('ys =', list(ys))
-        print()
+      # print()
 
-    # 縦横の縮尺を同じにする。
-    plt.axis('scaled')
-    plt.xlim(xmin=1-0.05, xmax=rate+0.05)
+    plt.xlim(xmin=0, xmax=max_redundancy+1)
     plt.ylim(ymin=-0.05, ymax=1.05)
     plt.grid(True)
 
