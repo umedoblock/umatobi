812671eba488f8b621e071f70771e20b7e391eff
umedoblock <devnull@localhost>
2012-12-03 15:58:13 +0900

    refactoring. (refs #135)

--- umatobi/tools/make_simulation_db.py
+++ umatobi/tools/make_simulation_db.py
@@ -34,6 +34,32 @@ def merge_client_dbs(client_dbs):
   #     print(record)
     return records
 
+def collect_client_dbs(watson_db):
+    client_dbs = []
+    client_rows = watson_db.select('clients', conditions='order by id')
+    for client_row in client_rows:
+        id_, num_nodes_ = (client_row[0], client_row[5])
+        print('id={}, num_nodes_={}'.format(id_, num_nodes_))
+      # print('id={}, num_nodes_={}'.format(type(id_), type(num_nodes_)))
+        client_db_path = \
+            simulation_db_path.replace(r'simulation.db',
+                                        'client.{}.db'.format(id_))
+        client_db = simulator.sql.SQL(db_path=client_db_path)
+        client_db.id, client_db.num_nodes = id_, num_nodes_
+        print('client_db =', client_db)
+        client_db.access_db()
+        print('client_db.id =', client_db.id)
+        client_dbs.append(client_db)
+
+    return client_dbs
+
+def count_nodes(client_dbs):
+    total_nodes = 0
+    for client_db in client_dbs:
+        total_nodes += client_db.num_nodes
+    print('total_nodes =', total_nodes)
+    return total_nodes
+
 if __name__ == '__main__':
     args = args_timestamp()
     simulation_db_path = get_xxx_path(args, 'db')
@@ -53,26 +79,9 @@ if __name__ == '__main__':
     print('watson_db =', watson_db)
     watson_db.access_db()
 
-    client_dbs = []
-    total_nodes = 0
-    client_rows = watson_db.select('clients', conditions='order by id')
-    for client_row in client_rows:
-        id_, num_nodes_ = (client_row[0], client_row[5])
-        print('id={}, num_nodes_={}'.format(id_, num_nodes_))
-      # print('id={}, num_nodes_={}'.format(type(id_), type(num_nodes_)))
-        client_db_path = \
-            simulation_db_path.replace(r'simulation.db',
-                                        'client.{}.db'.format(id_))
-        client_db = simulator.sql.SQL(db_path=client_db_path)
-        client_db.id, client_db.num_nodes = id_, num_nodes_
-        total_nodes += client_db.num_nodes
-        print('client_db =', client_db)
-        client_db.access_db()
-        print('client_db.id =', client_db.id)
-        client_dbs.append(client_db)
-    print('total_nodes =', total_nodes)
-
-    records = merge_client_dbs(client_dbs)
+    watson_db.client_dbs = collect_client_dbs(watson_db)
+    watson_db.total_nodes = count_nodes(watson_db.client_dbs)
+    watson_db.records = merge_client_dbs(watson_db.client_dbs)
 
   # print()
   # table_names = simulation_db.get_table_names()
