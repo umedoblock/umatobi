99b7a47ccac4fcfcc1924f77cdd5b623dcf366d0
umedoblock <devnull@localhost>
2014-01-19 00:47:05 +0900

    umatobi/tools 以下に配置する簡単な tcp 通信用 script。 r516 付近と関係していたはず。 (refs #202)

---
+++ umatobi/tools/tcp_client.py
@@ -0,0 +1,71 @@
+import threading
+import socket
+import argparse
+
+def arg():
+    parser = argparse.ArgumentParser(description='TCP simple client.')
+
+    parser.add_argument('--client', metavar='f', dest='client',
+                         nargs='?',
+                         default='0.0.0.0:0',
+                         help='example localhost:20000')
+    parser.add_argument('--recver', metavar='f', dest='recver',
+                         nargs='?',
+                         default='0.0.0.0:0',
+                         help='example localhost:30000')
+    parser.add_argument('--message', metavar='f', dest='message',
+                         nargs='?',
+                         default='',
+                         help='message')
+    args = parser.parse_args()
+
+    return args
+
+class TCPClient(threading.Thread):
+    def __init__(self, client, recver, message):
+        threading.Thread.__init__(self)
+        self.client = client
+        self.recver = recver
+        self.message = message
+        self.timeout_sec = 1
+
+        socket.setdefaulttimeout(self.timeout_sec)
+        self.sock_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock_recv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock_client.bind(self.client)
+        print('self.client =', self.client)
+        print('self.getsockname() =', self.sock_client.getsockname())
+
+    def do(self):
+        send_msg = self.message
+        self.sock_client.sendto(send_msg, self.recver)
+        client = self.sock_client.getsockname()
+        print('cleint is {}.'.format(client))
+        print('send to recver(={}) = {}'.format(self.recver, send_msg))
+
+        recved_msg = ''
+        recved_msg, who = self.sock_client.recvfrom(1024)
+
+        print('who =', who)
+        print('recved_msg =', recved_msg.decode())
+
+def get_host_port(host_port):
+    sp = host_port.split(':')
+    host = sp[0]
+    port = int(sp[1])
+    return host, port
+
+if __name__ == '__main__':
+    # how to use.
+    # tcp_client.py --recver=localhost:30000 \
+    #               --client=localhost:20000 \
+    #               --message='I am Client.'
+
+    args = arg()
+    client_ = get_host_port(args.client)
+    recver = get_host_port(args.recver)
+    message = args.message.encode()
+
+    client = TCPClient(client_, recver, message)
+    client.do()
+
---
+++ umatobi/tools/tcp_server.py
@@ -0,0 +1,64 @@
+import threading
+import socket
+import argparse
+
+def arg():
+    parser = argparse.ArgumentParser(description='TCP simple echo server.')
+
+    parser.add_argument('--server', metavar='f', dest='server',
+                         nargs='?',
+                         default='0.0.0.0:0',
+                         help='example localhost:20000')
+    args = parser.parse_args()
+
+    return args
+
+class TCPserver(threading.Thread):
+    def __init__(self, server):
+        threading.Thread.__init__(self)
+        self.server = server
+        self.timeout_sec = 1
+        self.n_clients = 4
+        self.socks = []
+asyncore.dispatcher_with_send
+#       socket.setdefaulttimeout(self.timeout_sec)
+        self.sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock_server.bind(self.server)
+        self.sock_server.listen(self.n_clients)
+        print('self.server =', self.server)
+        print('self.getsockname() =', self.sock_server.getsockname())
+
+    def run(self):
+        conn, addr = self.sock_server.accept()
+        self.sock_server.sendto(send_msg, self.recver)
+        server = self.sock_server.getsockname()
+        print('cleint is {}.'.format(server))
+        print('send to recver(={}) = {}'.format(self.recver, send_msg))
+
+        recved_msg = ''
+        recved_msg, who = self.sock_server.recvfrom(1024)
+
+        print('who =', who)
+        print('recved_msg =', recved_msg.decode())
+
+def get_host_port(host_port):
+    sp = host_port.split(':')
+    host = sp[0]
+    port = int(sp[1])
+    return host, port
+
+if __name__ == '__main__':
+    # how to use.
+    # tcp_server.py --recver=localhost:30000 \
+    #               --server=localhost:20000 \
+    #               --message='I am server.'
+
+    args = arg()
+    server_ = get_host_port(args.server)
+    recver = get_host_port(args.recver)
+    message = args.message.encode()
+
+    server = TCPserver(server_, recver, message)
+    server.do()
+
+
