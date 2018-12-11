f20e25db79805ef7b35f71f17cadf1da1ad8f96d
umedoblock <umedoblock@gmail.com>
2018-12-11 14:05:17 +0900

    write a utf-8 python scripts on ubuntu
    (refs #57)

---
+++ j_from_ubuntu.py
@@ -0,0 +1,6 @@
+# vim: set fileencoding=utf-8 :
+print("hello world !")
+print("あいうえお")
+
+# $ nkf -g ./j.py
+# UTF-8
---
+++ j_scratch_from_ubuntu.py
@@ -0,0 +1,3 @@
+# vim: set fileencoding=utf-8 :
+print("ubuntu の環境で作成した。")
+print("utf-8で作成している。")
