3a642874d2039d211d1c8c54ee7c144efd4394e7
umedoblock <umedoblock@gmail.com>
2018-12-11 18:23:42 +0900

    write watson basic test.
    (refs #53)

--- umatobi/test/test_suite.py
+++ umatobi/test/test_suite.py
@@ -3,14 +3,14 @@ from test.support import run_unittest
 
 from umatobi.test.test_lib import LibTests
 from umatobi.test.test_client import ClientTests
-# from umatobi.test import test_watson
+from umatobi.test.test_watson import WatsonTests
 
 def test_main():
     # see: Lib/test/test_math.py
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(LibTests))
     suite.addTest(LibTests('test_y15sformat_time')) # OK
- #  suite.addTest(unittest.MakeSuite(test_watson.WatsonTests))
+    suite.addTest(unittest.makeSuite(WatsonTests))
     suite.addTest(unittest.makeSuite(ClientTests))
     run_unittest(suite)
 
--- umatobi/test/test_watson.py
+++ umatobi/test/test_watson.py
@@ -1,30 +1,44 @@
 import os
-import sys
+import sys, shutil
 import threading
 import unittest
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from simulator.watson import Watson
-import p2p.core
-import simulator.node
+import umatobi.test
+from umatobi import SIMULATION_DIR
+from umatobi.simulator.watson import Watson
+from umatobi import lib
 
-test_dir = os.path.dirname(__file__)
+class WatsonTests(unittest.TestCase):
+    def setUp(self):
+        self.watson_office_addr = ('localhost', 65530)
+        self.simulation_seconds = 30
+        simulation_dir = os.path.join(".", SIMULATION_DIR)
+        self.log_level = 'INFO'
+        self.start_up_orig = lib.make_start_up_orig()
+        start_up_time = lib.y15sformat_time(self.start_up_orig)
+        dir_name = os.path.join(simulation_dir, start_up_time)
+        self.dir_name = dir_name
 
-class TestWatson(unittest.TestCase):
+        self.watson = Watson(self.watson_office_addr, self.simulation_seconds,
+                        self.start_up_orig,
+                        self.dir_name, self.log_level)
+
+    def tearDown(self):
+        # delete dbs, logs...
+        shutil.rmtree(self.watson.dir_name, ignore_errors=True)
+
     def test_watson_basic(self):
-        office = ('localhost', 65530)
-        simulation_seconds = 1
-        simulation_dir = os.path.join(test_dir, 'umatobi-simulation')
-        log_level = 'INFO'
+        watson = self.watson
+        watson = Watson(self.watson_office_addr, self.simulation_seconds,
+                        self.start_up_orig,
+                        self.dir_name, self.log_level)
 
-        self.assertFalse(os.path.exists(watson_log))
-        # watson for test
-        watson = Watson(office, simulation_seconds, simulation_dir, log_level)
+        expected_dir = os.path.join('.', SIMULATION_DIR, watson.start_up_time)
+        expected_path = os.path.join('.', SIMULATION_DIR, watson.start_up_time, 'simulation.db')
 
-        self.assertTrue(os.path.exists(watson_log))
-
-        watson.start()
-        watson.join()
+        self.assertEqual(self.watson_office_addr, watson.watson_office_addr)
+        self.assertEqual(expected_dir, watson.dir_name)
+        self.assertEqual(expected_path, watson.simulation_db_path)
 
 if __name__ == '__main__':
     unittest.main()
