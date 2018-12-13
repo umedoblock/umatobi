73899486c3b8500feba2eba39af78558ff126fb5
umedoblock <umedoblock@gmail.com>
2018-12-13 10:23:45 +0900

    darkness test

---
+++ umatobi/test/test_darknes.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import threading
+import unittest
+import queue
+
+import umatobi.simulator.darkness
+
+class TestDarkness(unittest.TestCase):
+    def test_first_node_id(self):
+        self.assertEqual(1, 1)
+
+if __name__ == '__main__':
+    unittest.main()
+
