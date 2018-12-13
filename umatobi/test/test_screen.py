9b5c1d24ce2ed69300ac6e3f7d297968a2a0cf04
umedoblock <umedoblock@gmail.com>
2018-12-13 09:26:50 +0900

    tiny tiny tiny mistake...
    (refs #82)

--- umatobi/test/test_screen.py
+++ umatobi/test/test_screen.py
@@ -4,11 +4,10 @@ import unittest
 from umatobi.test import *
 from umatobi.simulator.screen import Screen
 
-class TheaterTests(unittest.TestCase):
+class ScreenTests(unittest.TestCase):
     def test_screen_instance(self):
         screen = Screen(sys.argv)
         self.assertIsInstance(screen, Screen)
 
 if __name__ == '__main__':
     unittest.main()
-
