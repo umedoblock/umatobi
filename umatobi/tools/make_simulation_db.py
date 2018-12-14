49694d3ffb1cf2467fb7ec7561700cdd51d6b528
umedoblock <umedoblock@gmail.com>
2018-12-14 13:04:56 +0900

    Remove making nodes table to simulation.db
    (closed #96)

--- umatobi/tools/make_simulation_db.py
+++ umatobi/tools/make_simulation_db.py
@@ -87,8 +87,6 @@ def watson_make_simulation_db(simulation_db):
     simulation_db.inserts('growings', tuple(growings))
     simulation_db.commit()
 
-    init_nodes_table(simulation_db)
-
     for client_db in reversed(simulation_db.client_dbs):
         client_db.close()
     simulation_db.close()
