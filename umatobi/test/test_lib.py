67667500e5bb6749b7f00106ed2869afc5317082
umedoblock <umedoblock@gmail.com>
2018-12-20 18:11:49 +0900

    Add compledx test to keycmp()
    Sugeeee !
    unbelievable my program ability
    (refs #110)

--- umatobi/test/test_lib.py
+++ umatobi/test/test_lib.py
@@ -123,5 +123,75 @@ class LibTests(unittest.TestCase):
         self.assertLess(keycmp(key7fff, keyffff), 0)    # + 8000
         self.assertLess(keycmp(keyfffe, keyffff), 0)    # -1
 
+    def test_keycmp_cross(self):
+        key0000 = b'\x00' * KEY_OCTETS
+        keyffff = b'\xff' * KEY_OCTETS
+
+        key1fff = b'\x1f' + b'\xff' * (KEY_OCTETS-1)
+        key2000 = b'\x20' + b'\x00' * (KEY_OCTETS-1)
+        key2001 = b'\x20' + b'\x00' * (KEY_OCTETS-2) + b'\x01'
+
+        key5fff = b'\x5f' + b'\xff' * (KEY_OCTETS-1)
+        key6000 = b'\x60' + b'\x00' * (KEY_OCTETS-1)
+        key6001 = b'\x60' + b'\x00' * (KEY_OCTETS-2) + b'\x01'
+
+        key9fff = b'\x9f' + b'\xff' * (KEY_OCTETS-1)
+        keya000 = b'\xa0' + b'\x00' * (KEY_OCTETS-1)
+        keya001 = b'\xa0' + b'\x00' * (KEY_OCTETS-2) + b'\x01'
+
+        keydfff = b'\xdf' + b'\xff' * (KEY_OCTETS-1)
+        keye000 = b'\xe0' + b'\x00' * (KEY_OCTETS-1)
+        keye001 = b'\xe0' + b'\x00' * (KEY_OCTETS-2) + b'\x01'
+
+        self.assertEqual(KEY_OCTETS, 32)
+        self.assertEqual(len(key0000), KEY_OCTETS)
+        self.assertEqual(len(keyffff), KEY_OCTETS)
+        self.assertEqual(len(key1fff), KEY_OCTETS)
+        self.assertEqual(len(key2000), KEY_OCTETS)
+        self.assertEqual(len(key2001), KEY_OCTETS)
+        self.assertEqual(len(key5fff), KEY_OCTETS)
+        self.assertEqual(len(key6000), KEY_OCTETS)
+        self.assertEqual(len(key6001), KEY_OCTETS)
+        self.assertEqual(len(key9fff), KEY_OCTETS)
+        self.assertEqual(len(keya000), KEY_OCTETS)
+        self.assertEqual(len(keya001), KEY_OCTETS)
+        self.assertEqual(len(keydfff), KEY_OCTETS)
+        self.assertEqual(len(keye000), KEY_OCTETS)
+        self.assertEqual(len(keye001), KEY_OCTETS)
+
+        # Greater means Red, Less means Green, Eq means White
+
+        self.assertEqual(keycmp(key2000, key2000), 0)   # eq     White
+        self.assertGreater(keycmp(key2001, key2000), 0) # + 1    Red
+        self.assertGreater(keycmp(key9fff, key2000), 0) # + 7fff Red
+        self.assertLess(keycmp(keya000, key2000), 0)    # + 8000 Green
+        self.assertLess(keycmp(key1fff, key2000), 0)    # - 1    Green
+        self.assertLess(keycmp(keyffff, key2000), 0)    # end    Green
+        self.assertLess(keycmp(key0000, key2000), 0)    # zero   Green
+
+        self.assertEqual(keycmp(key6000, key6000), 0)   # eq     White
+        self.assertGreater(keycmp(key6001, key6000), 0) # + 1    Red
+        self.assertGreater(keycmp(keydfff, key6000), 0) # + 7fff Red
+        self.assertLess(keycmp(keye000, key6000), 0)    # + 8000 Green
+        self.assertLess(keycmp(key5fff, key6000), 0)    # - 1    Green
+        self.assertLess(keycmp(keyffff, key6000), 0)    # end    Green
+        self.assertLess(keycmp(key0000, key6000), 0)    # zero   Green
+
+        self.assertEqual(keycmp(keya000, keya000), 0)   # eq     White
+        self.assertGreater(keycmp(keya001, keya000), 0) # + 1    Red
+        self.assertGreater(keycmp(key1fff, keya000), 0) # + 7fff Red
+        self.assertLess(keycmp(key2000, keya000), 0)    # + 8000 Green
+        self.assertLess(keycmp(key9fff, keya000), 0)    # - 1    Green
+        self.assertGreater(keycmp(keyffff, keya000), 0) # end    Red
+        self.assertGreater(keycmp(key0000, keya000), 0) # zero   Red
+
+        self.assertEqual(keycmp(keye000, keye000), 0)   # eq     White
+        self.assertGreater(keycmp(keye001, keye000), 0) # + 1    Red
+        self.assertGreater(keycmp(key5fff, keye000), 0) # + 7fff Red
+        self.assertLess(keycmp(key6000, keye000), 0)    # + 8000 Green
+        self.assertLess(keycmp(keydfff, keye000), 0)    # - 1    Green
+        self.assertGreater(keycmp(keyffff, keye000), 0) # end    Red
+        self.assertGreater(keycmp(key0000, keye000), 0) # zero   Red
+
 if __name__ == '__main__':
     unittest.main()
