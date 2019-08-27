c5c18c51ede81bbb454ee52a1bc31aa43ffa1920
umedoblock@UmeBook <umedoblock@gmail.com>
2019-08-27 18:44:48 +0900

    refa
    (refs #139)

--- umatobi/tests/unit/test_client.py
+++ umatobi/tests/unit/test_client.py
@@ -25,7 +25,7 @@ class ClientTests(unittest.TestCase):
         client = Client(watson_office_addr, num_nodes)
 
         self.assertEqual(watson_office_addr, client.watson_office_addr)
+        self.assertEqual(num_nodes, client.num_nodes)
 
 if __name__ == '__main__':
     unittest.main()
-        self.assertEqual(num_nodes, client.num_nodes)
