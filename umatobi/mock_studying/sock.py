b96f98eaf76eb085d94452939e332555236d5643
umedoblock@UmeBook <umedoblock@gmail.com>
2019-09-01 15:07:58 +0900

    c._tcp_sock.connect.assert_called_with(watson_office_addr)
    (refs #147)

--- umatobi/mock_studying/sock.py
+++ umatobi/mock_studying/sock.py
@@ -8,6 +8,4 @@ class Client(object):
         self._tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #       print(f"{self}._make_contact_with(), {self._tcp_sock}.connect(={self.watson_office_addr})")
-#       self._tcp_sock.connect(self.watson_office_addr)
-
-
+        self._tcp_sock.connect(self.watson_office_addr)
--- umatobi/mock_studying/test_sock.py
+++ umatobi/mock_studying/test_sock.py
@@ -19,3 +19,24 @@ class ClientTestCase(unittest.TestCase):
 
         mock_socket.assert_called_with(socket.AF_INET, socket.SOCK_STREAM)
       # mock_contact_with.assert_called_with('localhost', 11111)
+
+#   @mock.patch.object(socket.socket, 'connect', autospec=True)
+#   @mock.patch.object(socket, 'socket', spec=socket)
+#   def test_socket3(self, mock_socket, mock_socket_connect):
+#       client = Client(('localhost', 11111))
+#       client._make_contact_with()
+
+#       print(client._tcp_sock)
+#       mock_socket.assert_called_with(socket.AF_INET, socket.SOCK_STREAM)
+#       mock_socket.return_value.attribute = socket.socket
+#       mock_socket_connect.assert_called_with(client._tcp_sock, 'localhost', 11111)
+
+  # @mock.patch('sock.socket.socket')
+
+    def test_socket4(self):
+      # https://stackoverflow.com/questions/31864168/mocking-a-socket-connection-in-python
+        with unittest.mock.patch('sock.socket.socket'):
+            watson_office_addr = ('localhost', 11111)
+            c = Client(watson_office_addr)
+            c._make_contact_with()
+            c._tcp_sock.connect.assert_called_with(watson_office_addr)
