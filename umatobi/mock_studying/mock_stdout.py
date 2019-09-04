d66e1e386528d6938b46053a614c219a1ef339f8
umedoblock@UmeBook <umedoblock@gmail.com>
2019-09-04 21:39:41 +0900

    study

---
+++ umatobi/mock_studying/mock_stdout.py
@@ -0,0 +1,13 @@
+from io import StringIO
+
+from unittest.mock import patch
+
+def foo():
+    print('Something')
+
+@patch('sys.stdout', new_callable=StringIO)
+def test(mock_stdout):
+    foo()
+    assert mock_stdout.getvalue() == 'Something1\n'
+
+test()
