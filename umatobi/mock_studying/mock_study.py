50813111c3a373cdb5a6f48588fa27186a47ec5e
umedoblock@UmeBook <umedoblock@gmail.com>
2019-09-07 09:04:00 +0900

    I got a power.
    (refs #147)

--- umatobi/mock_studying/mock_study.py
+++ umatobi/mock_studying/mock_study.py
@@ -12,6 +12,9 @@ class Foo(object):
     def sock2(self):
         pass
 
+    def tes(self):
+        return 'I am tes().'
+
 if __name__ == '__main__':
     print('hello')
     m = MagicMock()
@@ -20,6 +23,13 @@ if __name__ == '__main__':
     foo = Foo()
     with patch.object(foo, 'sock', 'sock with') as mocked:
         print('foo.sock =', foo.sock)
+
     with patch.object(foo, 'sock2', return_value='sock2() with') as mocked:
         print('foo.sock2() =', foo.sock2())
 
+    foo3 = Foo()
+    with patch.object(Foo, 'tes') as mocked_tes:
+        mocked_tes.return_value = 'mocked_tes.return_value'
+        print('foo3.tes() =', foo3.tes())
+    print('go out from with')
+    print('foo3.tes() =', foo3.tes())
