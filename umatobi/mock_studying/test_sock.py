466fb5e348b729fe96c3233008ff9c049ac32731
umedoblock@UmeBook <umedoblock@gmail.com>
2019-09-08 23:30:58 +0900

    refactoring
    (refs #155)

--- umatobi/mock_studying/test_sock.py
+++ umatobi/mock_studying/test_sock.py
@@ -11,30 +11,41 @@ class ClientTestCase(unittest.TestCase):
     @mock.patch.object(Client, '_make_contact_with', autospec=True)
     def test_socket(self, mock_contact_with):
         client = Client(('localhost', 11111))
+        self.assertEqual(client.watson_office_addr, ('localhost', 11111))
+
         client._make_contact_with()
         mock_contact_with.assert_called_with(client)
+        # ...
 
     #  socket(): OK
-    # connect(): ?
+    # connect(): OK
     @mock.patch.object(socket, 'socket', autospec=True)
     def test_socket2(self, mock_socket):
-        client = Client(('localhost', 11111))
-        client._make_contact_with()
+        watson_office_addr = ('localhost', 11111)
+        client = Client(watson_office_addr)
+        self.assertEqual(client.watson_office_addr, watson_office_addr)
 
+        client._make_contact_with()
         mock_socket.assert_called_with(socket.AF_INET, socket.SOCK_STREAM)
+        client._tcp_sock.connect.assert_called_with(watson_office_addr)
+        self.assertIsInstance(client._tcp_sock, type(mock_socket.return_value))
 
     #  socket(): OK!
     # connect(): OK
     @mock.patch('sock.socket', autospec=True)
     def test_socket4(self, mock_sock):
       # https://stackoverflow.com/questions/31864168/mocking-a-socket-connection-in-python
-        with unittest.mock.patch('sock.socket.socket'):
-            watson_office_addr = ('localhost', 11111)
-            c = Client(watson_office_addr)
-            c._make_contact_with()
-            mock_sock.socket.assert_called_with(sock.socket.AF_INET, sock.socket.SOCK_STREAM)
-            self.assertTrue(c._tcp_sock)
-            c._tcp_sock.connect.assert_called_with(watson_office_addr)
+        watson_office_addr = ('localhost', 11111)
+        c = Client(watson_office_addr)
+        self.assertEqual(c.watson_office_addr, watson_office_addr)
+
+        c._make_contact_with()
+        mock_sock.socket.assert_called_with(sock.socket.AF_INET, sock.socket.SOCK_STREAM)
+        self.assertTrue(c._tcp_sock)
+        c._tcp_sock.connect.assert_called_with(watson_office_addr)
+        self.assertIsInstance(c._tcp_sock, type(mock_sock.socket.return_value))
+        # This test share view point with test_socket2().
+        # delete with statement...
 
     #  socket(): OK
     # connect(): OK
@@ -47,6 +58,7 @@ class ClientTestCase(unittest.TestCase):
         c._make_contact_with()
         mock_sock_socket.socket.assert_called_with(sock.socket.AF_INET, sock.socket.SOCK_STREAM)
         c._tcp_sock.connect.assert_called_once_with(watson_office_addr)
+        # equal to test_socket4() ...
 
     def test_socket7(self):
         watson_office_addr = ('localhost', 11111)
