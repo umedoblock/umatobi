0dfeec254c1349649da2d1edaee74d54de1202e5
umedoblock@UmeBook <umedoblock@gmail.com>
2019-09-08 22:37:33 +0900

    mock sample
    (refs #155)

--- umatobi/mock_studying/sock.py
+++ umatobi/mock_studying/sock.py
@@ -4,6 +4,12 @@ class Client(object):
     def __init__(self, watson_office_addr):
         self.watson_office_addr = watson_office_addr
 
+    def _make_contact_with_raise(self):
+        raise()
+
+        self._tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._tcp_sock.connect(self.watson_office_addr)
+
     def _make_contact_with(self):
         self._tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
