1ded491175acb168c7fc241930b5defdcbd0f10d
umedoblock@UmeBook <umedoblock@gmail.com>
2019-09-09 22:29:40 +0900

    add test_steal_master_palm_logger_info(self, mock_path)
    by test mocked committee
    (refs #155)

--- umatobi/tests/unit/test_node.py
+++ umatobi/tests/unit/test_node.py
@@ -3,11 +3,16 @@ import sys, shutil
 import threading, socket
 import unittest, time
 import math, queue, pickle
+from unittest.mock import patch
+from unittest import mock
 
+import umatobi
+from umatobi.lib import *
 from umatobi.tests import *
 from umatobi.log import logger
 from umatobi.simulator.core.key import Key
 from umatobi.simulator.node import Node, NodeOffice
+from umatobi.simulator.node import Node
 from umatobi.simulator.node import NodeOpenOffice, NodeUDPOffice
 from umatobi import lib
 from umatobi.lib import current_y15sformat_time, make_start_up_orig
@@ -90,6 +95,33 @@ class NodeTests(unittest.TestCase):
 
         node2.release()
         node3.release()
+
+    @patch('os.path')
+    def test_steal_master_palm_logger_info(self, mock_path):
+        mock_path.isfile.return_value = False
+
+        node = self.node
+
+        self.master_hand_path = '/tmp/none'
+        with self.assertLogs('umatobi', level='DEBUG') as cm:
+            ret = node._steal_master_palm()
+        self.assertIsNone(ret)
+        mock_path.isfile.assert_called_with(node.master_hand_path)
+        self.assertRegex(cm.output[0], f"^INFO:umatobi:not found 'master_hand_path={node.master_hand_path}'")
+
+    def test_steal_master_palm_logger_info2(self):
+        node = self.node
+
+        patcher = patch('os.path')
+        mock_path = patcher.start()
+        mock_path.isfile.return_value = False
+
+        with self.assertLogs('umatobi', level='INFO') as cm:
+            ret = node._steal_master_palm()
+        self.assertIsNone(ret)
+        mock_path.isfile.assert_called_with(node.master_hand_path)
+        self.assertRegex(cm.output[0], f"^INFO:umatobi:not found 'master_hand_path={node.master_hand_path}'")
+        patcher.stop()
 
     def test_regist(self):
         node = self.node
