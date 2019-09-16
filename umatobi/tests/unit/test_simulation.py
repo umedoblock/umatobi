90a3997a97ce44c0a588644f75d5aa48b6100a0c
umedoblock@UmeBook <umedoblock@gmail.com>
2019-09-16 22:42:40 +0900

    add test_not_found_simulation_conf()
    (refs #151)

--- umatobi/tests/unit/test_simulation.py
+++ umatobi/tests/unit/test_simulation.py
@@ -9,5 +9,8 @@ class SimulationTests(unittest.TestCase):
         host_port = "127.0.0.1:22222"
         self.assertEqual(("127.0.0.1", 22222), get_host_port(host_port))
 
+    def test_not_found_simulation_conf(self):
+        path = 'umatobi/umatobi/simulator/simulation.conf'
+
 if __name__ == '__main__':
     unittest.main()
