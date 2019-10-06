b94effa042a737f7ab98fa62a214bea4e10b74ac
umedoblock@UmeBook <umedoblock@gmail.com>
2019-10-06 21:17:44 +0900

    for yaml load test
    (refs #175)

--- umatobi/studying_decorator_fixture.py
+++ umatobi/studying_decorator_fixture.py
@@ -1,3 +1,5 @@
+import yaml
+
 from umatobi.simulator.sql import SQL
 from umatobi.tests import *
 
@@ -16,7 +18,17 @@ def debug(f1):
     print('--')
     return inner
 
-def fixture(schema, table_name, key=None):
+def load_yaml_test1(yaml_path, key):
+    with open(yaml_path) as f:
+        y = yaml.load(f, Loader=yaml.SafeLoader)
+      # y = yaml.safe_load(f, Loader=yaml.SafeLoader)
+    print('y =', y)
+
+def fixture(yaml_path, key):
+    with open(yaml_path) as f:
+        y = yaml.load(f, Loader=yaml.SafeLoader)
+      # y = yaml.safe_load(f, Loader=yaml.SafeLoader)
+    print('y =', y)
   # def inner(db, table, key, value):
     def inner(db, table, key, value):
         row = db.select('* from table where key=value')
@@ -34,17 +46,21 @@ def facto(num):
 def func(num):
     print(f'func(num={num})')
 
-def sample():
-    sql = SQL(db_path=TEST_DB_PATH, schema_path=TEST_SCHEMA_PATH)
-    sql.create_db()
-    sql.create_table('test_table')
-
-    sql.remove_db()
+# @fixture('test.yaml', 'foo')
+# def sample():
+#     sql = SQL(db_path='fixture.db', schema_path=TEST_SCHEMA_PATH)
+#     sql.create_db()
+#     sql.create_table('test_table')
+# 
+#     sql.remove_db()
 
 if __name__ == '__main__':
-    facto(5)
-    print()
+#   facto(5)
+#   print()
 
-    print(func(6))
+#   print(func(6))
 
-    sample()
+#   sample()
+
+    load_yaml_test1(TEST_YAML_PATH + ".1", 'foo')
+#   fixture(TEST_YAML_PATH, 'foo')
