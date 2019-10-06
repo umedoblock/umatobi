8453683dc83de9766dbce8f7ff5af6afdee46133
umedoblock@UmeBook <umedoblock@gmail.com>
2019-10-06 22:58:18 +0900

    add TEST_YAML_PATH, TEST_YAML constants
    (refs #175)

--- umatobi/tests/constants.py
+++ umatobi/tests/constants.py
@@ -23,10 +23,12 @@ constants.LOGGER_STREAM = open(os.path.join(TESTS_PATH, 'stdout.log'), 'w')
 # tests original
 TEST_SCHEMA = 'test.schema'
 TEST_DB = 'test.db'
+TEST_YAML = 'test.yaml'
 FIXTURES_DIR = 'fixtures'
 FIXTURES_DIR_PATH = os.path.join(TESTS_PATH, FIXTURES_DIR)
 TEST_SCHEMA_PATH = os.path.join(FIXTURES_DIR_PATH, TEST_SCHEMA)
 TEST_DB_PATH = os.path.join(FIXTURES_DIR_PATH, TEST_DB)
+TEST_YAML_PATH = os.path.join(FIXTURES_DIR_PATH, TEST_YAML)
 
 RE_Y15S = r'20\d{2}-[01]\d{1}-[0123]\dT[012]\d[0-5]\d[0-5]\d'
 RE_ISO8601 = r'20\d{2}-[01]\d{1}-[0123]\dT[012]\d:[0-5]\d:[0-5]\d\.\d{6}'
