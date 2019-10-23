9cbfe9d30e28a16ef38fc64a38b7e9a1c2bf2652
umedoblock@UmeBook <umedoblock@gmail.com>
2019-10-23 21:21:05 +0900

    test skelton of test_simulation.py
    (refs #151)

--- umatobi/tests/unit/test_simulation.py
+++ umatobi/tests/unit/test_simulation.py
@@ -5,6 +5,13 @@ from umatobi.tests import *
 from umatobi.lib import get_host_port
 
 class SimulationTests(unittest.TestCase):
+
+    def test_make_client(self):
+        pass
+
+    def test_args_(self):
+        pass
+
     def test_get_host_port(self):
         host_port = "127.0.0.1:22222"
         self.assertEqual(("127.0.0.1", 22222), get_host_port(host_port))
