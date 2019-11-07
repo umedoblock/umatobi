89b5b28ab58d142fa279aac3a90dbf0456fbb7af
umedoblock@UmeBook <umedoblock@gmail.com>
2019-11-07 21:39:43 +0900

    organize and rectify var in constants.py
    (refs #195)

--- umatobi/constants.py
+++ umatobi/constants.py
@@ -1,7 +1,14 @@
 import os, sys
 
+# abspath suffix is "_PATH"
+# dirname suffix is "_DIR"
+
 # for package
 SIMULATOR_DIR = 'simulator'
+
+# must replace to appropriate value
+ATAT_SIMULATION_TIME = '@@SIMULATION_TIME@@'
+ATAT_N = '@@N@@'
 
 # for simulation
 SIMULATION_DIR = 'umatobi-simulation'
@@ -9,23 +16,22 @@ SIMULATION_DB = 'simulation.db'
 SIMULATION_SCHEMA = 'simulation.schema'
 WATSON_DB = 'watson.db'
 MASTER_PALM = 'master_palm.txt'
-ATAT_N = '@@N@@'
 CLIENT_N_DB = f'client.{ATAT_N}.db'
 
+# These paths are relative to UMATOBI_ROOT_PATH..
 UMATOBI_ROOT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+SIMULATION_SCHEMA_PATH = os.path.join(UMATOBI_ROOT_PATH, SIMULATOR_DIR, SIMULATION_SCHEMA)
+SIMULATION_SCHEMA_ORIG = os.path.join(UMATOBI_ROOT_PATH, SIMULATOR_DIR, SIMULATION_SCHEMA)
+
+# These paths are relative to SIMULATOR_DIR.
 SIMULATION_ROOT_PATH = os.path.join(os.path.expanduser('~'), SIMULATION_DIR)
+SIMULATION_DIR_PATH = os.path.join(SIMULATION_ROOT_PATH, ATAT_SIMULATION_TIME)
 
 # ROOT_DIR_PATH/umatobi-simulation/20190919T211132/simulation.schema
 #                                                 /simulation.db
 #                                                 /watson.db
 #                                                 /client.1.db
 #                                                 /master_palm.txt
-
-# abspath suffix is "_PATH"
-ATAT_SIMULATION_TIME = '@@SIMULATION_TIME@@'
-SIMULATION_DIR_PATH = os.path.join(SIMULATION_ROOT_PATH, ATAT_SIMULATION_TIME)
-SIMULATION_SCHEMA_PATH = os.path.join(UMATOBI_ROOT_PATH, SIMULATOR_DIR, SIMULATION_SCHEMA)
-SIMULATION_SCHEMA_ORIG = os.path.join(UMATOBI_ROOT_PATH, SIMULATOR_DIR, SIMULATION_SCHEMA)
 
 #   %(relativeCreated)d Time in ms when the LogRecord was created,
 LOGGER_FMT = '%(relativeCreated)d %(name)s %(levelname)s %(filename)s %(funcName)s() - %(message)s'
