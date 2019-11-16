ca1205fbad54229fc7e5f52885406fcc4ec496d9
umedoblock@UmeBook <umedoblock@gmail.com>
2019-11-16 09:49:34 +0900

    test skelton
    (refs #199, #196)

--- umatobi/tests/unit/test_make_simulation_db.py
+++ umatobi/tests/unit/test_make_simulation_db.py
@@ -6,8 +6,26 @@ class MakeSimulationDbTests(unittest.TestCase):
     def setUp(self):
         self.start_up_orig = ClientTests.start_up_orig
 
-    def test_make_simulation_db(self):
-        self.assertTrue(True)
+    def test_collect_client_dbs(self):
+        pass
+
+    def test_count_nodes(self):
+        pass
+
+    def test__select_client_db(self):
+        pass
+
+    def test_merge_client_dbs(self):
+        pass
+
+    def test_watson_make_simulation_db(self):
+        pass
+
+    def test_init_nodes_table(self):
+        pass
+
+    def test_init_nodes_table2(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
