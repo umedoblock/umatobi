405c6d692398e8b3907910f1022135b30c49327a
umedoblock@UmeBook <umedoblock@gmail.com>
2019-11-16 22:14:24 +0900

    setUpClass(cls), tearDownClass(cls), cls.outsider_db
    (refs #199)

--- umatobi/tests/unit/test_make_simulation_db.py
+++ umatobi/tests/unit/test_make_simulation_db.py
@@ -1,12 +1,25 @@
 import unittest
 
+import umatobi
+from umatobi.tests import *
 from umatobi.lib import *
+from umatobi.simulator.sql import SQL
 
 class MakeSimulationDbTests(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         cls.start_up_orig = SimulationTime()
+
+        cls.outsider_db = SQL(db_path=TEST_SIMULATION_DB_PATH,
+                              schema_path=SIMULATION_SCHEMA_PATH)
+       #cls.outsider_db.create_db()
+       #cls.outsider_db.access_db()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.outsider_db.close()
+        cls.outsider_db.remove_db()
 
     def setUp(self):
         self.start_up_orig = MakeSimulationDbTests.start_up_orig
