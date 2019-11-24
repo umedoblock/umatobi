37d6cade87828ab33de9bf146995588167f2f84c
umedoblock@UmeBook <umedoblock@gmail.com>
2019-11-24 22:05:50 +0900

    easily repair test_client_with_watson.py
    (refs #99)

--- umatobi/tests/integration/test_client_with_watson.py
+++ umatobi/tests/integration/test_client_with_watson.py
@@ -4,31 +4,26 @@ import shutil
 from umatobi.tests import *
 from umatobi.simulator.client import Client
 from umatobi.simulator.watson import Watson
-from umatobi import lib
+from umatobi.lib import *
 
 class ClientStoryTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.start_up_orig = SimulationTime()
+      # set_simulation_schema(cls.start_up_orig)
+
     def setUp(self):
         watson_office_addr = ('localhost', 0)
-        simulation_seconds = 3
+        simulation_seconds = 30
         log_level = 'DEBUG'
 
-        simulation_dir = SIMULATION_DIR
-        start_up_orig = lib.make_start_up_orig()
-        start_up_time = lib.y15sformat_time(start_up_orig)
-
-        dir_name = os.path.join(simulation_dir, start_up_time)
-
-        if not os.path.isdir(dir_name):
-            print(f"os.makedirs(dir_name={dir_name}, exist_ok=True)")
-            os.makedirs(dir_name, exist_ok=True)
+        start_up_orig = ClientStoryTests.start_up_orig
 
         self.watson = Watson(watson_office_addr, simulation_seconds,
-                        start_up_orig,
-                        dir_name, log_level)
+                             start_up_orig, log_level)
 
     def tearDown(self):
-        # delete dbs, logs...
-        shutil.rmtree(self.watson.dir_name, ignore_errors=True)
+        pass
 
     # client emurate client.start()
     # watson emurate watson.run()
@@ -44,11 +39,12 @@ class ClientStoryTests(unittest.TestCase):
 
         # emulate client.start()
         client = Client(watson.watson_office_addr, num_nodes)
-        client.consult_watson()
 
-        client._make_growings_table()
+        client._consult_watson()
 
-        client._start_darkness()
+        client._has_a_lot_on_mind()
+
+        client._confesses_darkness()
 
       ######################################################################
       # watson.relaxing() ##################################################
@@ -63,16 +59,20 @@ class ClientStoryTests(unittest.TestCase):
         watson.watson_tcp_office.shutdown()
         watson._wait_client_db()
 
-        client._wait_break_down()
+        client._waits_to_break_down()
 
 #       # Client 終了処理開始。
-        client._release()
+        client._run_a_way()
 
-        client.close()
+        client._come_to_a_bad_end()
+
+        client._thanks()
+
         # emulate client.start() done !
 
         watson.simulation_db.access_db()
         watson._merge_db_to_simulation_db()
+        watson._create_simulation_table()
         watson._construct_simulation_table()
         watson.simulation_db.close()
 
